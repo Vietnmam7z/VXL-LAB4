e0aae51b9d803c6e2b8dc22d636e7d72efa8f9e8
DESKTOP-7Z\Genki <Genki4000@gmail.com>
2024-10-30 18:04:57 +0700

    Init for protus

--- Scheduler/Debug/Core/Src/global.d
+++
@@ -1,8 +0,0 @@
-Core/Src/global.o: ../Core/Src/global.c ../Core/Inc/global.h \
- ../Core/Inc/input_reading.h ../Core/Inc/timer.h
-
-../Core/Inc/global.h:
-
-../Core/Inc/input_reading.h:
-
-../Core/Inc/timer.h:
